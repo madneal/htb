cd0a96070c5ee5276ff4f21cb14ceb1ece50d43e
root <neal1991@sina.com>
2019-07-29 14:20:26 +0000

    sort quote by id

---
+++ Haystack/parse.py
@@ -0,0 +1,16 @@
+import json
+result = {}
+txt = ""
+with open("quotes.json") as f:
+  data = f.readlines()
+  for ele in data:
+    obj = json.loads(ele)
+    id = int(obj["_id"])
+    result[id] = obj["_source"]["quote"]
+  for i in sorted(result.keys()):
+    print(i)
+    txt = txt + result[i]
+with open("result.md", "w") as f1:
+  f1.write(txt)
+#print(result)
+

